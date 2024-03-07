480f349240e5242d2be6d92ead34e398eb3e90d2
MahmoudKazlak <mahmoudkazlak2@gmail.com>
2024-03-07 15:08:50 +0200

    Task16 Submittion

--- js/script.js
+++ js/script.js
@@ -1,60 +1,44 @@
-$(document).ready(function() {
-    var searchInput = $("#searchInput");
-    searchInput.autocomplete({
-        minLength: 3,
-        source: function(request, response) {
-            $.ajax({
-                url: "https://en.wikipedia.org/w/api.php",
-                dataType: "jsonp",
-                data: {
-                    action: "opensearch",
-                    format: "json",
-                    search: request.term
-                },
-                success: function(data) {
-                    response(data[1]);
-                }
-            });
+let lastSearchedUser = '';
+let lastSearchedUserId = '';
+$("#searchForm").submit(function (event) {
+    event.preventDefault();
+    var searchInput = $("#searchInput").val();
+    //scroll
+    if (searchInput === lastSearchedUser) {
+        var lastRow = $(".col-md-12.text-light:last-of-type");
+        if (lastRow.length > 0) {
+            $('html, body').scrollTop(lastRow.offset().top);
         }
-    });
-    $("#searchForm").submit(function(event) {
-        event.preventDefault();
-        var searchTerm = searchInput.val();
-        $.ajax({
-            url: "https://en.wikipedia.org/w/api.php",
-            dataType: "jsonp",
-            data: {
-                action: "query",
-                format: "json",
-                list: "search",
-                srsearch: searchTerm,
-                srlimit: 5,
-                srinfo: "totalhits",
-                srprop: "snippet"
-            },
-            success: function(data) {
-                var results = data.query.search;
-                displaySearchResults(results);
-            }
-        });
-    });
-    searchInput.keypress(function(event) {
-        if (event.keyCode === 13) { 
-            $(this).autocomplete("close");
-            $("#searchForm").submit(); 
+        return;
+    }
+    //fetch
+    fetch("https://api.github.com/users/" + searchInput)
+    .then(response => response.json())
+    .then(response => displaySearchResults(response));
+    //display
+    function displaySearchResults(response) {
+        var searchResults = document.getElementById("searchResults");
+        var searchRow = document.createElement("div");
+        searchRow.classList.add("col-md-12", "text-light");
+        var userId = response['id'];
+        searchRow.setAttribute("id", "i" + userId);
+        if (response['created_at']) { //exist
+            searchRow.innerHTML = `
+                ${response['avatar_url'] ? `<img class="mb-3" style="width:150px" src="${response['avatar_url']}">` : ''}
+                ${response['name'] ? `<p><strong>Name:</strong> ${response['name']}</p>` : ''}
+                ${response['company'] ? `<p><strong>Company:</strong> ${response['company']}</p>` : ''}
+                ${response['email'] ? `<p><strong>Email:</strong> ${response['email']}</p>` : ''}
+                ${response['followers'] !== null ? `<p><strong>Followers:</strong> ${response['followers']}</p>` : ''}
+                ${response['following'] !== null ? `<p><strong>Following:</strong> ${response['following']}</p>` : ''}
+                ${response['public_repos'] !== null ? `<p><strong>Public Repos:</strong> ${response['public_repos']}</p>` : ''}
+                <div class="row">
+                <div class="col-md-12 mt-1 border-success">
+                <hr class="line-edit border-success">
+                </div>
+                </div> `;
+            searchResults.appendChild(searchRow);
         }
-    });
-
-    function displaySearchResults(results) {
-        var html = "<ul >";
-        if (results.length > 0) {
-            results.forEach(function(result) {
-                html += "<li ><a href='https://en.wikipedia.org/wiki/" + encodeURIComponent(result.title) + "' target='_blank'>" + result.title + "</a> - " + result.snippet + "</li>";
-            });
-        } else {
-            html += "<li >No results found</li>";
-        }
-        html += "</ul>";
-        $("#searchResults").html(html);
+        lastSearchedUser = searchInput;
+        lastSearchedUserId = userId;
     }
 });
