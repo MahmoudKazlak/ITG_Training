31664b35f196b2a40f18f1e86b60f0efa8964580
MahmoudKazlak <mahmoudkazlak2@gmail.com>
2024-03-11 13:50:28 +0200

    To Do List

--- js/script.js
+++ js/script.js
@@ -1,67 +1,37 @@
-let lastSearchedUser = '';
-let lastSearchedUserId = '';
-let existingRows = {};
-
-$("#searchForm").submit(function (event) {
-    event.preventDefault();
-    var searchInput = $("#searchInput").val();
-
-    //check
-    if (searchInput === lastSearchedUser) {
-        var lastRow = $("#i" + lastSearchedUserId);
-        if (lastRow.length > 0) {
-            $("#searchResults tbody").append(lastRow);
-            $('html, body').animate({
-                scrollTop: lastRow.offset().top
-            }, 0);
+$(document).ready(function() {
+    function displayCheck(element) {
+        $(element).find('.fa-check').addClass("d-block").removeClass("d-none");
+        $(element).css("color","white");    
+        $(element).css("background-color", "gray");
+        $(element).css("text-decoration", "line-through");
+    }    
+    function deleteTask(element) {
+        $(element).closest('li').remove();
+    }
+    $("#myToDoList").submit(function(event) {
+        event.preventDefault();
+        var newTaskCont = $("#newTask").val().trim();
+        if(newTaskCont.length > 0) {
+            $("#list-group").append(`
+                <li class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+                    <div class="d-flex align-items-center">
+                        <i class="fa-solid fa-check text-success d-none"></i>
+                        <span class="ms-5">${newTaskCont}</span>
+                    </div>
+                    <a href="#!" class="delete-task" data-mdb-toggle="tooltip" title="Remove item">
+                        <i class="fas fa-times text-primary"></i>
+                    </a>
+                </li>
+            `);
+            $("#newTask").val("");
         }
-        $("#searchInput").val('');
-        return;
-    }
-    //check
-    if (existingRows[searchInput]) {
-        var existingRow = $("#i" + existingRows[searchInput].id);
-        existingRow.appendTo("#searchResults tbody");
-        $('html, body').animate({
-            scrollTop: existingRow.offset().top
-        }, 0);
-        $("#searchInput").val('');
-        return;
-    }
+    });
 
-    //fetch
+    $("#list-group").on("click", "li", function() {
+        displayCheck(this);
+    });
 
-    fetch("https://api.github.com/users/" + searchInput) 
-        .then(response => response.json())
-        .then(response => {
-            displaySearchResults(response);
-            existingRows[searchInput] = response; 
-        });
+    $("#list-group").on("click", ".delete-task", function(event) {
+        deleteTask(this);
+    });
 });
-
-//display
-function displaySearchResults(response) {
-    var searchResults = $("#searchResults tbody");
-    var searchRow = $("<tr>").addClass("");
-    var userId = response['id'];
-    searchRow.attr("id", "i" + userId);
-    if (response['created_at']) { 
-        searchRow.append(`
-            <td>${response['avatar_url'] ? `<img style="width:100%"  src="${response['avatar_url']}">` : ''}</td>
-            <td>${response['name'] ? response['name'] : ''}</td>
-            <td>${response['company'] ? response['company'] : ''}</td>
-            <td>${response['email'] ? response['email'] : ''}</td>
-            <td>${response['followers'] !== null ? response['followers'] : ''}</td>
-            <td>${response['following'] !== null ? response['following'] : ''}</td>
-            <td>${response['public_repos'] !== null ? response['public_repos'] : ''}</td>
-        `);
-        searchResults.append(searchRow);
-        $('html, body').animate({
-            scrollTop: searchRow.offset().top
-        }, 0);
-    }
-    //clear
-    $("#searchInput").val('');
-    lastSearchedUser = searchInput;
-    lastSearchedUserId = userId;
-}
