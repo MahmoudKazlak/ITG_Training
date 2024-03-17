fe041d33157ed07059314dfe736aaf539110eee2
MahmoudKazlak <mahmoudkazlak2@gmail.com>
2024-03-17 15:16:58 +0200

    Acessability

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ var obj = {
     productName: "Hugo Boss men's suit",
     productPrice: 2500,
     productPriceFormatted: "DKK 2.500,00",
-    inStock: !0, productImg: "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
+    inStock: true,
+    productImg: "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
     href: "/riley-heeled-buckle-boots/shoes-accessories/new-arrivals/572383?uniqueId=572383",
     swatches: [{
       swatchDefAttCode: "BLCK",
@@ -12,13 +13,12 @@ var obj = {
       swatchAttrName: "color",
       img: { src: "https://akamai-scene7.garnethill.com/is/image/garnethill/48611_BLCK_SW?$SLI_MiniSwatch$" }
     }]
-
   }, {
     productID: "KL2432",
     productName: "Hugo Boss men's suit",
     productPrice: 2500,
     productPriceFormatted: "DKK 2.500,00",
-    inStock: !0,
+    inStock: true,
     productImg: "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
     href: "/riley-heeled-buckle-boots/shoes-accessories/new-arrivals/572383?uniqueId=572383",
     swatches: [{
@@ -34,28 +34,42 @@ var obj = {
       img: { src: "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_MAHO_SW?$SLI_MiniSwatch$" }
     }]
   }]
-},
-  productContainer = document.getElementById("productContainer");
-obj.results.forEach(function (a) {
-  if (a.inStock) {
-    var t = document.createElement("div"); t.classList.add("col-lg-4", "product-card"), t.setAttribute("role", "listitem"), t.innerHTML = `
-            <div class="card">
-            <div class="flex-sm-row  d-sm-flex d-lg-block">
-              <div class="col-lg-12 col-sm-6">
-                <img src="${a.productImg}" class="card-img-top" alt="${a.productName}">
-              </div>
-              <div class="col-lg-12 col-sm-6">
-                <div class="card-body">
-                  <h5 class="card-title">${a.productName}</h5>
-                  <p class="card-text mt-4">Price: ${a.productPriceFormatted}</p>
-                  <div class="d-flex flex-wrap mt-4">
-                    ${a.swatches.map(a => `<img src="${a.img.src}" class="swatch-img" alt="${a.swatchName}">`).join("")}
-                  </div>
-                  <a href="${a.href}" class="btn btn-primary mt-5" aria-label="Buy ${a.productName} now">Buy Now</a>
-                </div>
-              </div>
-            </div>
+};
+var productContainer = document.getElementById("productContainer");
+obj.results.forEach(function (product) {
+  if (product.inStock) {
+    var productCard = document.createElement("div");
+    productCard.classList.add("col-lg-4", "product-card");
+    productCard.setAttribute("role", "listitem"); 
+    productCard.setAttribute("tabindex", "0");
+    productCard.innerHTML = `
+      <div class="card">
+        <figure class="card-img-top-container">
+          <img
+            src="${product.productImg}"
+            class="card-img-top"
+            alt="${product.productName}"
+            aria-label="This image is describing the ${product.productName} "
+            aria-hidden="false">
+        </figure>
+        <div class="card-body">
+          <h5 class="card-title">${product.productName}</h5>
+          <p class="card-text mt-4">Price: ${product.productPriceFormatted}</p>
+          <div class="d-flex flex-wrap mt-4">
+            ${product.swatches.map(swatch => `
+              <img
+                src="${swatch.img.src}"
+                class="swatch-img"
+                alt="${swatch.swatchName} swatch"
+                aria-label="Color ${swatch.swatchName}"
+                data-color="${swatch.swatchDefAttCode}"
+              >
+            `).join("")}
           </div>
-            `, productContainer.appendChild(t)
+          <a href="${product.href}" class="btn btn-primary mt-5" aria-label="Buy ${product.productName} now">Buy Now</a>
+        </div>
+      </div>
+    `;
+    productContainer.appendChild(productCard);
   }
 });
